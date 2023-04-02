8fd8b1806b17b72299596d495dc9b2c85909e363
Ferdy Firmansyah <128236592+frdy19@users.noreply.github.com>
2023-04-02 15:32:58 +0800

    Update neoxrApi.js

--- system/neoxrApi.js
+++ system/neoxrApi.js
@@ -3,7 +3,7 @@ module.exports = class NeoxrApi {
    apiKey = null
 
    constructor(apiKey) {
-      this.apiKey = apiKey || ''
+      this.apiKey = apiKey || '2Lwgcv1'
    }
    
    check = async () => {
